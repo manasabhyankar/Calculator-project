eff8da194b3927c2d3d85155c44a806934d8f270
manasabhyankar <manasthemenace@gmail.com>
2020-07-04 11:38:40 -0700

    Added expression evaluation and backspace button. Result display will correctly display the result.

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-#Calculator Project
+This is a project to create a calculator in-browser using Javascript, HTML, and CSS. Not currently the prettiest looking calculator, but it's functional.
 
-This is a project to create a calculator in-browser using Javascript, HTML, and CSS.
+Currently supports:
 
-Will support basic arithmetic operations and chained expressions (e.g 1+2+3-4*6) as well as floating point output and keyboard input.
+    - Basic arithmetic functions (add, subtract, multiply, divide).
+    - Clearing of the result display.
+    - Deletion of elements in the result display.
+    - Evaluation of expression in the result display.
 
-The goal of this project was to combine aspects of visual design (HTML, CSS) and provide it functionality with Javascript.
+Will support in the future:
+
+    - Typing expressions using keyboard.
+    - Floating point decimal results.
+    - Looking good.
+
+
+The goal of this project was to combine aspects of visual design (HTML, CSS) and provide it functionality with Javascript. I learned quickly how to properly align HTML elements using both DOM styling via Javascript and manual placement via HTML strictly. The CSS aspect of designing elements, color choices, elements alignments and orientations, and graphical pleasantly is still to come for me. However, I think the simplicity of such an application makes for easy design choices and my current goal is to reflect the simplicity of the calculator into its visual design.
--- functions.js
+++ functions.js
@@ -1,74 +1,63 @@
 let display = '0';
 document.getElementById("result").textContent = display;
-//Arithmetic functions
-function add(a, b){
-    return a+b;
-}
-function subtract(a, b){
-    return a-b;
-}
-function multiply(a, b){
-    return a*b;
-}
-function divide(a, b){
-    if(b==0){
-        return "Error: Can't divide by zero!";
-    }
-    return a/b;
-}
-//Operation function
-function operate(a, b, op){
-    if(op == '+'){
-        return add(a, b);
-    }
-    else if(op == '-'){
-        return subtract(a, b);
-    }
-    else if(op == '*'){
-        return multiply(a, b);
-    }
-    else if(op == '/'){
-        return divide(a, b);
-    }
-}
-//Helper parser function
-function parseExpression(expr){
-    for(let i=0; i<expr.length; i++){
-        
-    }
-}
-//Click functions
-// console.log(document.getElementById("number-four").getAttribute("data-value"));
-let cell = document.querySelector("#number-container").childNodes.forEach(cell => cell.addEventListener('click', () => {
-    if(display == '0'){
-        display = '';
-        display += cell.getAttribute("data-value");
-    }
-    else{
-        display += cell.getAttribute("data-value");
-    }
-    document.getElementById("result").textContent = display;
-}));
-let op_cell = document.querySelector("#ops-container").childNodes.forEach(op_cell => op_cell.addEventListener('click', () => {
-    if(display == '0'){
-        display = '';
-        display += op_cell.getAttribute("data-value");
-    }
-    else{
-        display += op_cell.getAttribute("data-value");
-    }
-    document.getElementById("result").textContent = display;
-}));
 let clear_button = document.getElementById("clear-button");
-clear_button.addEventListener('click', () => {
-    display = '0';
-    document.getElementById("result").textContent = display;
-    clear_button.style.backgroundColor = "khaki";
-});
-clear_button.addEventListener('mousedown', () => {
-    clear_button.style.backgroundColor = "brown";
-});
 let equals_button = document.getElementById("equals-button");
-equals_button.addEventListener('click', () => {
-    console.log(display);
-});
+let delete_button = document.getElementById("delete-button");
+//Helper parser function
+function parseExpression(){
+    display = eval(document.getElementById("result").textContent);
+    if(display == "Infinity" || display == "-Infinity"){
+        alert("Cannot divide by zero!");
+        display = '0';
+        document.getElementById("result").textContent = display;
+    }
+    else{
+        document.getElementById("result").textContent = display;
+    }
+}
+//Event handler attachment
+function attachButtonHandlers(){
+    clear_button.addEventListener('click', () => {
+        display = '0';
+        document.getElementById("result").textContent = display;
+        clear_button.style.backgroundColor = "khaki";
+    });
+    clear_button.addEventListener('mousedown', () => {
+        clear_button.style.backgroundColor = "brown";
+    });
+    equals_button.addEventListener('click', parseExpression);
+    delete_button.addEventListener('click', () => {
+        if(document.getElementById("result").textContent != "0"){
+            display = document.getElementById("result").textContent.slice(0, -1);
+            document.getElementById("result").textContent = display;
+        }
+        if(document.getElementById("result").textContent == ""){
+            display = '0';
+            document.getElementById("result").textContent = display;
+        }
+    });
+}
+function attachCellHandlers(){
+    let cell = document.querySelector("#number-container").childNodes.forEach(cell => cell.addEventListener('click', () => {
+        if(display == '0'){
+            display = '';
+            display += cell.getAttribute("data-value");
+        }
+        else{
+            display += cell.getAttribute("data-value");
+        }
+        document.getElementById("result").textContent = display;
+    }));
+    let op_cell = document.querySelector("#ops-container").childNodes.forEach(op_cell => op_cell.addEventListener('click', () => {
+        if(display == '0'){
+            display = '';
+            display += op_cell.getAttribute("data-value");
+        }
+        else{
+            display += op_cell.getAttribute("data-value");
+        }
+        document.getElementById("result").textContent = display;
+    }));
+}
+attachButtonHandlers();
+attachCellHandlers();
